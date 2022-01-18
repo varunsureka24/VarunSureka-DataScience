5fa2bf5c2a92a0de1a29aea0592817a76911f18a
varun <varunsureka123@gmail.com>
2022-01-18 08:58:31 -0600

    com

--- mario.py
+++ mario.py
@@ -1,18 +1,22 @@
-marioSize = input("what size would you like the tower: ")
-size = int(marioSize)
 
-while size < 0:
-        marioSize = input("what size would you like the tower: ")
-        size = int(marioSize)
-
-for i in range(size, 0, -1):
-    for spaces in range(i - 1, 0, -1):
-        print(" ", end="")
-    for star in range(0,(size - i), 1):
-        print("*", end="")
-    print("  ",end = "")
-    for star in range(0,(size - i), 1):
-        print("*", end="")
-    for spaces in range(i - 1, 0, -1):
-        print(" ", end="")
-    print()
+while True:
+    marioSize = input("what size would you like the tower: ")
+    size = int(marioSize) + 1
+    if (size == 0):
+        print("goodbye, thank you for playing")
+        break
+    if (size < 2 or size > 26):
+        print("bad number")
+    else:
+        for i in range(size, 0, -1):
+            for spaces in range(i - 1, 0, -1):
+                print(" ", end="")
+            for star in range(0,(size - i), 1):
+                print("*", end="")
+            print("  ",end = "")
+            for star in range(0,(size - i), 1):
+                print("*", end="")
+            for spaces in range(i - 1, 0, -1):
+                print(" ", end="")
+            print()
+    
