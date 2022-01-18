882dfde7e6cb797d55e66c95f3f1a7ebadc3559c
varun <varunsureka123@gmail.com>
2022-01-18 09:53:54 -0600

    COM

---
+++ stringMethods.py
@@ -0,0 +1,26 @@
+string = "hello, my name is varun"
+#functions for string
+print(string.capitalize()) ####
+# print(string.center(20))
+# print(string.encode())
+h = "v\ta\tr\tu\tn\t"
+print(h.expandtabs(6)) ####
+# print(string.format())
+print(string.index('my')) ####
+# print(string.isalpha())
+# print(string.isdigit())
+# print(string.islower())
+# print(string.isprintable())
+# print(string.istitle())
+# print(" ".join(string))
+# print(string.lower())
+h = string.maketrans("m", "h") #####
+print(string.translate(h))
+print(string.replace("varun","ekaum")) ####
+# print(string.rindex("varun"))
+# print(string.rpartition("hello"))
+# print(string.rstrip())
+# print(string.splitlines())
+# print(string.strip())
+# print(string.title())
+# print(string.upper())
